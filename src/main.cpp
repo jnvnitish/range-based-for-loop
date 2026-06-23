56f814bda8f77af80f435a2db11fea3e7358565a
jnvnitish <jnvnitish4930@gmail.com>
2026-06-23 08:57:30 +0530

    add range-based for loop

---
+++ src/main.cpp
@@ -0,0 +1,45 @@
+//section9
+//rangr-based for loop
+#include <iostream>
+#include <vector>
+#include <iomanip>
+using namespace std;
+ int main(){
+
+
+
+//     int scores[] {10,20,30};
+//     for(auto score:scores)
+//     cout<<score<<endl;
+//  }
+
+
+
+// vector<double> temperatures{23.4,45.6,67.54,75.6};
+// double average_temp{};
+// double total{};
+// for(auto temp:temperatures)
+// total+=temp;
+// if(temperatures.size()!=0)
+// average_temp=total/temperatures.size();
+// cout<<fixed<<setprecision(2)<<endl;
+// cout<<"the average temperature is "<<average_temp<<endl;
+// }
+
+
+
+// for(auto val:{1,2,3,4,5})
+// cout<<val<<endl;
+// }
+
+// for (auto c: "Hi, this is me")
+// if(c!=' ')
+// cout<<c;
+// }
+
+for(auto c:"Hi this is me")
+if(c==' ')
+cout<<"\t";
+else
+cout<<c;
+ }
